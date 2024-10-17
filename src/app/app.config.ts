39477a6b712dc44d15fbde555e5254e95176f7a2
carlos-montano-hub <montanoc70@gmail.com>
2024-10-17 14:10:22 -0700

    added localization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
+  ],
 };
---
+++ src/app/features/language-button/language-button.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './language-button.component.html',
+  styleUrl: './language-button.component.css',
+})
+export class LanguageButtonComponent {
+  currentLanguage = 'EN';
+  languages = ['EN', 'ES', 'FR', 'DE'];
+
+  changeLanguage() {
+    const currentIndex = this.languages.indexOf(this.currentLanguage);
+    const nextIndex = (currentIndex + 1) % this.languages.length;
+    this.currentLanguage = this.languages[nextIndex];
+    console.log(`Language changed to ${this.currentLanguage}`);
+  }
+}
